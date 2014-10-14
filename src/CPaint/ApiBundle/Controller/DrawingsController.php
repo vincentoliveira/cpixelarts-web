b497d93b2a6f020c2270485e127f63db444ded10
vincentoliveira <vincent@oliveira.fr>
2014-10-14 15:44:29 +0200

    Api: Get drawings/Get drawing

--- src/CPaint/ApiBundle/Controller/DrawingsController.php
+++ src/CPaint/ApiBundle/Controller/DrawingsController.php
@@ -3,20 +3,56 @@
 namespace CPaint\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use FOS\RestBundle\View\View;
 
 class DrawingsController extends Controller
 {
+    /**
+     * Get paginated drawing list.
+     * 
+     * Optional parameters:
+     * - page (default: 1)
+     * - max_results (default: 10)
+     * 
+     * @api
+     */
     public function getDrawingsAction()
     {
-        $data = array('drawings' => []);
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('CPaintDrawingBundle:Drawing');
+
+        $page = $this->get('request')->query->get('page', 1);
+        $maxResults = $this->get('request')->query->get('max_results', 10);
+        $data = $repo->findAllPaginated($page, $maxResults);
 
         $view = View::create()
             ->setData($data);
 
         return $this->getViewHandler()->handle($view);
     }
+    
+    /**
+     * Get drawing by id
+     * 
+     * @api
+     */
+    public function getDrawingAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('CPaintDrawingBundle:Drawing');
+        $drawing = $repo->find($id);
+
+        if (null === $drawing) {
+            throw new NotFoundHttpException(sprintf("Drawing with id '%s' could not be found.", $id));
+        }
+        
+        $view = View::create()
+            ->setData(array('drawing' => $drawing));
+
+        return $this->getViewHandler()->handle($view);
+    }
+
 
     /**
      * @return \FOS\RestBundle\View\ViewHandler
--- src/CPaint/DrawingBundle/Repository/DrawingRepository.php
+++ src/CPaint/DrawingBundle/Repository/DrawingRepository.php
@@ -19,5 +19,28 @@ class DrawingRepository extends EntityRepository
     {
         return $this->createQueryBuilder('drawing')->getQuery();
     }
+
+    /**
+     * Find all query
+     * 
+     * @return \Doctrine\ORM\Query
+     */
+    public function findAllPaginated($page = 1, $maxResults = 10)
+    {
+        $qb = $this->createQueryBuilder('drawing');
+        
+        $count = $qb->select('count(drawing)')->getQuery()->getSingleScalarResult();
+        
+        $qb->select('drawing');
+        $qb->setFirstResult((abs($page) - 1) * $maxResults);
+        $qb->setMaxResults($maxResults);
+        $results = $qb->getQuery()->getArrayResult();
+        
+        return array(
+            'results' => $results,
+            'count' => intval($count),
+            'page' => $page,
+        );
+    }
     
 }
