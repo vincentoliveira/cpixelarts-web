a33f5b99ac8a235681c3d63c6e8ad1dea8f46e3e
vincentoliveira <vincent@oliveira.fr>
2014-10-29 15:53:07 +0100

    Drawing without reloading

--- src/CPaint/DefaultBundle/Resources/public/js/main.js
+++ src/CPaint/DefaultBundle/Resources/public/js/main.js
@@ -2,14 +2,13 @@ $(document).ready(function() {
     /** Responsive drawing **/
     function respondDrawing() {
         if ($("#drawing").length === 0) {
-            console.log("No drawing");
             return;
         }
-        
+
         var width = $("#drawing").width() - 42;
         var dWidth = $("#drawing").attr("drawing-width");
         var pWidth = Math.floor(width / dWidth);
-        
+
         $("#drawing").find(".pixel").css("width", pWidth + "px").css("height", pWidth + "px");
         $("#drawing").find(".line:first .pixel").css("height", (pWidth + 20) + "px");
         $("#drawing").find(".line:last .pixel").css("height", (pWidth + 20) + "px");
@@ -19,15 +18,21 @@ $(document).ready(function() {
     $(window).resize(respondDrawing);
     respondDrawing();
     /** End responsibe drawing **/
-    
+
+    // get selected color
+    function getSelectedColor()
+    {
+        return $('.color.selected').css("background-color");
+    }
+
     // select a color
     $(".color").click(function(e) {
         e.preventDefault();
         $(".color").removeClass("selected");
         $(this).addClass("selected");
-        $("#drawing").find(".pixel").css("border-color", $(this).css("background-color"));
+        $("#drawing").find(".pixel").css("border-color", getSelectedColor());
         $('input[name="color"]').val($(this).attr("data-id"));
-        
+
         // if selected color is 42 => set background to white
         if ($(this).attr("data-id") == 42) {
             $('body').css('background-color', '#fff');
@@ -35,26 +40,53 @@ $(document).ready(function() {
             $('body').css('background-color', '#4992aa');
         }
     });
+    
+    // empty pixel on hover
+    $(".empty-pixel").hover(function(){
+        if ($(this).hasClass("empty-pixel")) {
+            $(this).css('background', getSelectedColor());
+        }
+    },function(){
+        if ($(this).hasClass("empty-pixel")) {
+            $(".empty-pixel").css('background', 'none');
+        }
+    });
+    
     // if selected color is 42 => set background to white
     if ($('.color.selected').attr("data-id") == 42) {
         $('body').css('background-color', '#fff');
     }
-    
+
     // add a pixel
     $(".empty-pixel").click(function(e) {
-        $(".empty-pixel").removeClass("selected");
-        $(this).addClass("selected");
+        $(this).removeClass("empty-pixel").css('background', getSelectedColor());
         $('input[name="position"]').val($(this).attr("data-pos"));
-        $("#addPixelForm").submit();
+        if ($("#addPixelForm").attr("reload") == "true") {
+            $("#addPixelForm").submit();
+        } else {
+            var datas = $("#addPixelForm").serialize();
+            $.ajax({
+                type: $("#addPixelForm").attr('method'),
+                url: $("#addPixelForm").attr('action'),
+                data: datas,
+                success: function(response) {
+                    $(".empty-pixel:hover").css('background-color', $(this).css("background-color"));
+                },
+                error: function() {
+                    alert("An error has occured");
+                    location.reload();
+                }
+            });
+        }
     });
-    
+
     // load bitmap
     $(".bitmap").each(function() {
         if ($(this).find("img").attr("bitmap-url")) {
             $(this).find("img").attr("src", $(this).find("img").attr("bitmap-url"));
         }
     });
-    
+
     // set title
     $(".name-it-link").click(function(e) {
         $(this).hide();
