9f0178fd88bbb266645d659dc3dda42367ce62c5
vincentoliveira <vincent@oliveira.fr>
2014-10-29 13:47:21 +0100

    Fix border in pixel table

--- src/CPaint/DefaultBundle/Resources/public/js/main.js
+++ src/CPaint/DefaultBundle/Resources/public/js/main.js
@@ -6,42 +6,47 @@ $(document).ready(function() {
             return;
         }
         
-        var width = $("#drawing").width();
+        var width = $("#drawing").width() - 42;
         var dWidth = $("#drawing").attr("drawing-width");
-        var pWidth = Math.floor(width / dWidth) + "px";
-        $("#drawing").find(".pixel").css('width', pWidth).css('height', pWidth);
+        var pWidth = Math.floor(width / dWidth);
+        
+        $("#drawing").find(".pixel").css("width", pWidth + "px").css("height", pWidth + "px");
+        $("#drawing").find(".line:first .pixel").css("height", (pWidth + 20) + "px");
+        $("#drawing").find(".line:last .pixel").css("height", (pWidth + 20) + "px");
+        $("#drawing").find(".line").find(".pixel:first").css("width", (pWidth + 20) + "px");
+        $("#drawing").find(".line").find(".pixel:last").css("width", (pWidth + 20) + "px");
     }
     $(window).resize(respondDrawing);
     respondDrawing();
     /** End responsibe drawing **/
     
     // select a color
-    $('.color').click(function(e) {
+    $(".color").click(function(e) {
         e.preventDefault();
-        $('.color').removeClass('selected');
-        $(this).addClass('selected');
-        $('#drawing').css('border-color', $(this).css('background-color'));
-        $('input[name="color"]').val($(this).attr('data-id'));
+        $(".color").removeClass("selected");
+        $(this).addClass("selected");
+        $("#drawing").find(".pixel").css("border-color", $(this).css("background-color"));
+        $('input[name="color"]').val($(this).attr("data-id"));
     });
     
     // add a pixel
-    $('.empty-pixel').click(function(e) {
-        $('.empty-pixel').removeClass('selected');
-        $(this).addClass('selected');
-        $('input[name="position"]').val($(this).attr('data-pos'));
+    $(".empty-pixel").click(function(e) {
+        $(".empty-pixel").removeClass("selected");
+        $(this).addClass("selected");
+        $('input[name="position"]').val($(this).attr("data-pos"));
         $("#addPixelForm").submit();
     });
     
     // load bitmap
-    $('.bitmap').each(function() {
+    $(".bitmap").each(function() {
         if ($(this).find("img").attr("bitmap-url")) {
             $(this).find("img").attr("src", $(this).find("img").attr("bitmap-url"));
         }
     });
     
     // set title
-    $('.name-it-link').click(function(e) {
+    $(".name-it-link").click(function(e) {
         $(this).hide();
-        $('.name-it-form').removeClass('hidden').show();
+        $(".name-it-form").removeClass("hidden").show();
     });
 });
